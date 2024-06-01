4256b92699f9219eecb09ba86f9854a54d1a824e
Swapneel Maulik <swapneelmoulik@gmail.com>
2024-06-01 13:47:16 +0530

    New updates

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import dotenv from "dotenv"
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config(
     {
         path: './env'
     }
 )
+
+connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(`Server is running at port : ${process.env.PORT} `);
+    })
+
+
+})
+.catch((err) => {
+    console.log("Mongo db connection failed", err);
+})
